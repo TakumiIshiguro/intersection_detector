0d2fae61c0564024c1fa3c64bd5c1f6d656db26b
TakumiIshiguro <aquarius1496@gmail.com>
2025-01-28 01:19:57 +0900

    rename and remove

--- experiment/learning.sh
+++
@@ -1,5 +0,0 @@
-for i in `seq 1`
-do
-  roslaunch intersection_detector nav.launch world_name:=tsudanuma2-3_v2.3.3.world map_file:=real_tsudanuma2-3_v2.yaml waypoints_file:=loop.yaml dist_err:=1.0 initial_pose_x:=-5.0 initial_pose_y:=7.7 initial_pose_a:=3.14 use_waypoint_nav:=true robot_x:=0.0 robot_y:=0.0 robot_Y:=0.0
-  sleep 10
-done
--- scripts/learning_intersection.sh
+++
@@ -1,4 +0,0 @@
-#please change your target rosbag
-rosbag play ~/bags/h.bag
-rosservice call /training_intersection "data: true"
-
